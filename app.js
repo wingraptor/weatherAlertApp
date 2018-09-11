66d474427ff381e0a77e87177394ce1e831545da
wingraptor <akonobrathwaite@gmail.com>
2018-09-11 15:00:05 -0400

    Add env variables for API URLS and PORT

--- app.js
+++ app.js
@@ -1,24 +1,20 @@
 var express = require("express"),
-    request = require("request"),
-    rp      = require("request-promise");
+  request = require("request"),
+  rp = require("request-promise");
 
 var app = express();
-
-
+require("dotenv").config();
 //Variable Declarations
 var parsedOpenWeatherBody = "",
-    bdsWeather            = "",
-    unixDate              = "";
-
+  bdsWeather = "",
+  unixDate = "";
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
 // HOMEPAGE ROUTE
 app.get("/", function(req, res) {
-  rp(
-    "http://api.openweathermap.org/data/2.5/weather?q=Bridgetown,BB&appid=0c07f5b25a6c999fb6684c3a582694a1&units=metric"
-  ).then(function(openWeatherBody) {
+  rp(process.env.WEATHER_API).then(function(openWeatherBody) {
     parsedOpenWeatherBody = JSON.parse(openWeatherBody);
     unixDate = new Date(parsedOpenWeatherBody.dt * 1000);
     barbadosWeather();
@@ -30,21 +26,21 @@ app.get("/", function(req, res) {
     });
   }
   function unsplashWallpaper() {
-    rp(
-      "https://api.unsplash.com/photos/random?client_id=27473b4801cf5f88a2a27c8c192d262f6252f89609ee60008a8b198f987242e5&query=barbados"
-    ).then(function(wallpaper) {
+    rp(process.env.UNSPLASH_API + "&query=barbados").then(function(wallpaper) {
       res.render("index", {
         weather: parsedOpenWeatherBody,
         unixDate: unixDate,
         weatherAlert: bdsWeather,
-        wallpaper:JSON.parse(wallpaper)
+        wallpaper: JSON.parse(wallpaper)
       });
     });
   }
 });
 
 // Tell Express to listen for requests on port 3000 (starts server)
-app.listen(3000, function() {
+app.listen(process.env.PORT, function() {
   //Logs "Server has started" to the console when server has been started
   console.log("Server has started");
 });
+
+
