f75ca29e1da8561b2ff05459ad4028bb96ed7218
wingraptor <akonobrathwaite@gmail.com>
2018-09-11 15:20:36 -0400

    Add process.env.IP to app.listen

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.get("/", function(req, res) {
 });
 
 // Tell Express to listen for requests on port 3000 (starts server)
-app.listen(process.env.PORT, function() {
+app.listen(process.env.PORT, process.env.IP, function() {
   //Logs "Server has started" to the console when server has been started
   console.log("Server has started");
 });
